6f5b2a66c2355efdb1b28875b353e04399948f6c
Furkan Salim <furkansalimm@gmail.com>
2022-08-03 11:57:07 +0300

    Set up linters and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
