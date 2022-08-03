31ea14449ddcdc721af25a16589bafedd209dea2
melaku05 <melakushiferaw05@gmail.com>
2022-08-03 15:12:41 +0300

    Add redirect path for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
   # Defines the root path route ("/")
